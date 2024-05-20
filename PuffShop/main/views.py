dbdf1bce3ae2c70262cf167d024c2910cab2daa6
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-05-20 18:05:10 +0300

    Creation html and css for models

--- PuffShop/main/views.py
+++ PuffShop/main/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Item
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    categories = Category.objects.all()
+
+    data = {
+        'categories': categories,
+    }
+
+    return render(request, 'main/index.html', data)
+
+def category_items(request, pk):
+    category = get_object_or_404(Category, id=pk)
+    items = Item.objects.filter(category=category)
+    context = {
+        'category': category,
+        'items': items
+    }
+    return render(request, 'main/category_items.html', context)
