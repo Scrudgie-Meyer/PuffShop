dac731d442b20634f0c475b1046975c8616e9b96
Waldemar Petrash <116195123+Scrudgie-Meyer@users.noreply.github.com>
2024-05-22 00:43:55 +0300

    Edit order admin + tests

--- PuffShop/main/admin.py
+++ PuffShop/main/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Item, Category, Order, OrderItem
 
-# Register your models here.
-admin.site.register(Item)
-admin.site.register(Category)
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
@@ -14,4 +11,16 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ['name', 'phone', 'address']
     inlines = [OrderItemInline]
 
+    def save_model(self, request, obj, form, change):
+        if obj.status == 'canceled' and obj.pk:
+            order_items = OrderItem.objects.filter(order=obj)
+            for order_item in order_items:
+                item = order_item.item
+                item.quantity += order_item.quantity
+                item.save()
+        super().save_model(request, obj, form, change)
+
+# Register your models here.
+admin.site.register(Item)
+admin.site.register(Category)
 admin.site.register(Order, OrderAdmin)
