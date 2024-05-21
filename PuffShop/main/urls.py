27b5aed7b586fb72aa1f9a4090fc2d58ac59d354
Volodimir Petrash <wofer151@gmail.com>
2024-05-21 18:42:45 +0300

    Add cart

--- PuffShop/main/urls.py
+++ PuffShop/main/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('items', views.items, name='items'),
     path('about_us', views.about_us, name='about_us'),
     path('contact', views.contact, name='contact'),
-    path('cart', views.cart, name='cart'), #Concatanate from user session
+    path('cart', views.cart, name='cart'),
+    path('add_<int:pk>/', views.add_to_cart, name='add_to_cart'),
+    path('delete_<int:pk>/', views.delete_from_cart, name='delete_from_cart'),
     path('category_<int:pk>/', views.category_items, name='category_items'),
     path('detail_<int:pk>/', views.detail, name='item_detail')
 ]
--- PuffShop/main/views.py
+++ PuffShop/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Item
 # Create your views here.
 
@@ -21,15 +21,14 @@ def category_items(request, pk):
 
 def detail(request, pk):
 
-    items = get_object_or_404(Item, id=pk)
-
+    item = get_object_or_404(Item, id=pk)
+    max_quantity = item.quantity
+    quantities = range(1, max_quantity + 1)
     context = {
-        'item': items
+        'item': item,
+        'quantities': quantities
     }
     return render(request, 'main/item_detail.html', context)
-
-def cart(request):
-    return render(request, 'main/cart.html')
 
 def items(request):
     return render(request, 'main/items.html')
@@ -39,3 +38,39 @@ def about_us(request):
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+def cart(request):
+    cart = request.session.get('cart', {})
+    total_price = sum(item['price'] * item['quantity'] for item in cart.values())
+    return render(request, 'main/cart.html', {'cart': cart, 'total_price': total_price})
+
+def add_to_cart(request, pk):
+    item = get_object_or_404(Item, id=pk)
+    quantity = int(request.GET.get('quantity', 1))
+    cart = request.session.get('cart', {})
+
+    if str(pk) in cart:
+        cart[str(pk)]['quantity'] += quantity
+    else:
+        cart[str(pk)] = {
+            'name': item.name,
+            'price': float(item.price),
+            'quantity': quantity
+        }
+
+    request.session['cart'] = cart
+    return redirect('cart')
+
+
+def delete_from_cart(request, pk):
+    item = get_object_or_404(Item, id=pk)
+    quantity = int(request.GET.get('quantity', 1))
+    cart = request.session.get('cart', {})
+
+    if str(pk) in cart:
+        cart[str(pk)]['quantity'] -= quantity
+        if cart[str(pk)]['quantity'] <= 0:
+            del cart[str(pk)]
+
+    request.session['cart'] = cart
+    return redirect('cart')
