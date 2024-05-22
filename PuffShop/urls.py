d66c9065589b889b029e4c0d2a6653457995b876
Waldemar Petrash <116195123+Scrudgie-Meyer@users.noreply.github.com>
2024-05-22 11:43:54 +0300

    Move styles to main

--- PuffShop/urls.py
+++ PuffShop/urls.py
@@ -6,7 +6,7 @@ Definition of urls for PuffShop.
 # from django.urls import path
 # from django.contrib import admin
 # from django.contrib.auth.views import LoginView, LogoutView
-# from app import forms, views
+# from main import forms, views
 
 
 # urlpatterns = [
